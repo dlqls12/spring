6471cc19541b02349955fac940b706a1838bb282
dlqls12 <dlqls12321@gmail.com>
2020-08-12 08:56:15 +0900

    댓글 작성시 첨부파일이 없으면 파일 업로드 로직 스킵

--- src/main/java/com/sbs/lyb/at/controller/FileController.java
+++ src/main/java/com/sbs/lyb/at/controller/FileController.java
@@ -1,13 +1,12 @@
 package com.sbs.lyb.at.controller;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,16 +19,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartRequest;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import com.google.common.base.Joiner;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.sbs.lyb.at.dto.File;
 import com.sbs.lyb.at.dto.ResultData;
 import com.sbs.lyb.at.service.FileService;
 import com.sbs.lyb.at.service.VideoStreamService;
 import com.sbs.lyb.at.util.Util;
-
-import reactor.core.publisher.Mono;
 
 @Controller
 public class FileController {
@@ -38,13 +37,21 @@ public class FileController {
 	@Autowired
 	private VideoStreamService videoStreamService;
 
-	@RequestMapping("/usr/file/streamVideo")
-	public Mono<ResponseEntity<byte[]>> streamVideo(
-			@RequestHeader(value = "Range", required = false) String httpRangeList, int id) {
-		File file = fileService.getFileById(id);
-		final ByteArrayInputStream is = new ByteArrayInputStream(file.getBody());
+	private LoadingCache<Integer, File> fileCache = CacheBuilder.newBuilder().maximumSize(100)
+			.expireAfterAccess(2, TimeUnit.MINUTES).build(new CacheLoader<Integer, File>() {
+				@Override
+				public File load(Integer fileId) {
+					return fileService.getFileById(fileId);
+				}
+			});
 
-		return Mono.just(videoStreamService.prepareContent(is, file.getFileSize(), file.getFileExt(), httpRangeList));
+	@RequestMapping("/usr/file/streamVideo")
+	public ResponseEntity<byte[]> streamVideo(@RequestHeader(value = "Range", required = false) String httpRangeList,
+			int id) {
+		File file = Util.getCacheData(fileCache, id);
+
+		return videoStreamService.prepareContent(new ByteArrayInputStream(file.getBody()), file.getFileSize(),
+				file.getFileExt(), httpRangeList);
 	}
 
 	@RequestMapping("/usr/file/doUploadAjax")
@@ -62,6 +69,12 @@ public class FileController {
 			String[] fileInputNameBits = fileInputName.split("__");
 
 			if (fileInputNameBits[0].equals("file")) {
+				byte[] fileBytes = Util.getFileBytesFromMultipartFile(multipartFile);
+				
+				if ( fileBytes == null || fileBytes.length == 0 ) {
+					continue;
+				}
+				
 				String relTypeCode = fileInputNameBits[1];
 				int relId = Integer.parseInt(fileInputNameBits[2]);
 				String typeCode = fileInputNameBits[3];
@@ -71,7 +84,6 @@ public class FileController {
 				String fileExtTypeCode = Util.getFileExtTypeCodeFromFileName(multipartFile.getOriginalFilename());
 				String fileExtType2Code = Util.getFileExtType2CodeFromFileName(multipartFile.getOriginalFilename());
 				String fileExt = Util.getFileExtFromFileName(multipartFile.getOriginalFilename()).toLowerCase();
-				byte[] fileBytes = Util.getFileBytesFromMultipartFile(multipartFile);
 				int fileSize = (int) multipartFile.getSize();
 
 				int fileId = fileService.saveFile(relTypeCode, relId, typeCode, type2Code, fileNo, originFileName,
@@ -82,7 +94,7 @@ public class FileController {
 		}
 
 		Map<String, Object> rsDataBody = new HashMap<>();
-		rsDataBody.put("fileIdsStr", Joiner.on("").join(fileIds));
+		rsDataBody.put("fileIdsStr", Joiner.on(",").join(fileIds));
 		rsDataBody.put("fileIds", fileIds);
 
 		return new ResultData("S-1", String.format("%d개의 파일을 저장했습니다.", fileIds.size()), rsDataBody);
